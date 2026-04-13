e7708f0f788930eaf646ebc27590d326db019ce3
Maria Eduarda <maria436557@gmail.com>
2026-04-12 21:06:37 -0300

    feat(controller): adiciona controllers base para entidades do sistema

--- petshower/src/main/java/br/com/petshower/controller/AtendimentoController.java
+++ petshower/src/main/java/br/com/petshower/controller/AtendimentoController.java
@@ -1,4 +1,43 @@
 package br.com.petshower.controller;
 
+import br.com.petshower.model.Atendimento;
+import br.com.petshower.service.AtendimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/atendimentos")
+
 public class AtendimentoController {
+
+    @Autowired
+    private AtendimentoService atendimentoService;
+
+    @PostMapping
+    public Atendimento criar(@RequestBody Atendimento atendimento) {
+        return  atendimentoService.criar(atendimento);
+    }
+
+    @PutMapping("/{id}")
+    public Atendimento atualizar(@PathVariable Long id, @RequestBody Atendimento atendimento) {
+        return atendimentoService.alterar(id, atendimento);
+    }
+
+    @GetMapping
+    public List<Atendimento> listar(){
+        return  atendimentoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Atendimento buscarPorId(@PathVariable("id") Long id){
+        return atendimentoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        atendimentoService.excluir(id);
+    }
+
 }
--- petshower/src/main/java/br/com/petshower/controller/ServicoController.java
+++ petshower/src/main/java/br/com/petshower/controller/ServicoController.java
@@ -1,4 +1,43 @@
 package br.com.petshower.controller;
 
+import br.com.petshower.model.Servico;
+import br.com.petshower.service.ServicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicos")
+
 public class ServicoController {
+
+    @Autowired
+    private ServicoService servicoService;
+
+    @PostMapping
+    public Servico criar(@RequestBody Servico cliente) {
+        return  servicoService.criar(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public Servico atualizar(@PathVariable Long id, @RequestBody Servico servico) {
+        return servicoService.alterar(id, servico);
+    }
+
+    @GetMapping
+    public List<Servico> listar(){
+        return  servicoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Servico buscarPorId(@PathVariable("id") Long id){
+        return servicoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        servicoService.excluir(id);
+    }
+
 }
